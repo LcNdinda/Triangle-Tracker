07cdbcd64930dbc7cebd7830b2b19341c5a81348
LcNdinda <lcndinda@gmail.com>
2018-08-10 09:30:58 +0300

    Add prompt box to input values

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var Tracker = function(sideA, sideB, sideC){
+
+  if (sideA<=0 || sideB<=0 || sideB<=0 ||) {
+    alert("Length of sides cannot be equal to or less than zero")
+  }
+  else {
+  alert("Enter another number")
+
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+var length1 = parseInt(prompt("Enter the length of First side"));
+var length2 = parseInt(prompt("Enter the length of Second side"));
+var length3 = parseInt(prompt("Enter the length of Third side"));
+
+var result = Tracker(length1, length2, length3);
