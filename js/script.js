756b777336414be8328553327c8ac25369c49d03
LcNdinda <lcndinda@gmail.com>
2018-08-10 10:46:53 +0300

    add logic flows

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ var Tracker = function(sideA, sideB, sideC){
   if (sideA<=0 || sideB<=0 || sideC<=0) {
     alert("Length of sides cannot be equal to or less than zero");
   }
-  else if (sideA==sideB && sideB == sideC) {
+  else if (sideA == sideB && sideB == sideC && sideA == sideC) {
     alert("The Triangle is an EQUILATERAL")
   }
 
   else if ((sideA == sideB) || (sideB == sideC) || (sideC == sideA)) {
-    alert("The Triangle is an ISOSCELES")
+    alert("The Triangle is an ISOSCELES");
   }
 
-  else if () {
+  else if (sideA !== sideB && sideB !== sideC && sideA !== sideC ) {
+    alert("The triangle is a SCALENE")
+  }
 
+  else if ((sideA + sideB < sideC) || (sideA + sideC < sideB) || (sideB + sideC < sideA) ) {
+    alert("This NOT A TRIANGLE")
   }
 
   else {
@@ -22,8 +26,8 @@ var Tracker = function(sideA, sideB, sideC){
 
 }
 
-var length1 = prompt("Enter the length of First side");
-var length2 = prompt("Enter the length of Second side");
-var length3 = prompt("Enter the length of Third side");
+var length1 = parseInt(prompt("Enter the length of First side"));
+var length2 = parseInt(prompt("Enter the length of Second side"));
+var length3 = parseint(prompt("Enter the length of Third side"));
 
 var result = Tracker(length1, length2, length3);
