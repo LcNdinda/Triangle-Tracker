10d32ca0c1cb5829f92e0fd40f4c16b49412bd55
LcNdinda <lcndinda@gmail.com>
2018-08-10 10:13:16 +0300

    add a function called Tracker

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
 var Tracker = function(sideA, sideB, sideC){
 
-  if (sideA<=0 || sideB<=0 || sideB<=0 ||) {
-    alert("Length of sides cannot be equal to or less than zero")
+  if (sideA<=0 || sideB<=0 || sideC<=0) {
+    alert("Length of sides cannot be equal to or less than zero");
   }
   else {
-  alert("Enter another number")
+  alert("Enter another number");
 
   }
-
-
-
 
 
 }
 
-
-
-
-
-
-
-
-
-var length1 = parseInt(prompt("Enter the length of First side"));
-var length2 = parseInt(prompt("Enter the length of Second side"));
-var length3 = parseInt(prompt("Enter the length of Third side"));
+var length1 = prompt("Enter the length of First side");
+var length2 = prompt("Enter the length of Second side");
+var length3 = prompt("Enter the length of Third side");
 
 var result = Tracker(length1, length2, length3);
