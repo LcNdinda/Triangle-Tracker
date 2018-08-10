18d0abd98ebb2b2f05f0af2e30071c1fa76c913e
LcNdinda <lcndinda@gmail.com>
2018-08-10 11:29:09 +0300

    Add triangle properties

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var Tracker = function(sideA, sideB, sideC){
+  if (sideA == "" || sideB == "" || sideC == "") {
+    alert("The side of a Triangle cannot be empty")
+  }
 
-  if (sideA<=0 || sideB<=0 || sideC<=0) {
+  else if (sideA<=0 || sideB<=0 || sideC<=0) {
     alert("Length of sides cannot be equal to or less than zero");
   }
   else if (sideA == sideB && sideB == sideC && sideA == sideC) {
@@ -11,16 +14,13 @@ var Tracker = function(sideA, sideB, sideC){
     alert("The Triangle is an ISOSCELES");
   }
 
-  else if (sideA !== sideB && sideB !== sideC && sideA !== sideC ) {
-    alert("The triangle is a SCALENE")
-  }
 
   else if ((sideA + sideB < sideC) || (sideA + sideC < sideB) || (sideB + sideC < sideA) ) {
     alert("This NOT A TRIANGLE")
   }
 
   else {
-    alert("Try again")
+    alert("The triangle is a SCALENE")
   }
 
 
@@ -28,6 +28,12 @@ var Tracker = function(sideA, sideB, sideC){
 
 var length1 = parseInt(prompt("Enter the length of First side"));
 var length2 = parseInt(prompt("Enter the length of Second side"));
-var length3 = parseint(prompt("Enter the length of Third side"));
+var length3 = parseInt(prompt("Enter the length of Third side"));
 
 var result = Tracker(length1, length2, length3);
+
+
+//SCALENE TRIANGLE
+// else if (sideA !== sideB && sideB !== sideC && sideA !== sideC ) {
+//   alert("The triangle is a SCALENE")
+// }
